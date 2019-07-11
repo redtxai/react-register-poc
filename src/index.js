e2eb4440ea4d0433eef6ead6aeb014dabe981d54
redtxai <redtxai@gmail.com>
2019-07-11 17:50:45 -0300

    refactor(App) Change file format to jsx

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import './index.css';
-import App from './App.js';
+import App from './App';
 import rootReducer from './reducers';
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
